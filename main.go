7ebdd7a5005bf764f476b20a9eb60eddd5db1eef
PavelBozhek <themolodets2570@gmail.com>
2024-02-28 20:42:18 +0500

    Добавлена генерация сессии пользователя, а так же отдельные структуры для чтения данных

--- main.go
+++ main.go
@@ -11,6 +11,17 @@ import (
 	_ "github.com/lib/pq"
 )
 
+type RegistrationRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 type User struct {
 	ID       int    `json:"id"`
 	Username string `json:"username"`
@@ -31,20 +42,20 @@ func main() {
 
 	// Маршрут для регистрации новых пользователей
 	router.POST("/register", func(c *gin.Context) {
-		var user User
-		if err := c.BindJSON(&user); err != nil {
+		var req RegistrationRequest
+		if err := c.BindJSON(&req); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
 			return
 		}
 
 		// Хэширование пароля
-		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
 			return
 		}
 
-		user.Password = string(hashedPassword)
+		user := User{Username: req.Username, Email: req.Email, Password: string(hashedPassword)}
 
 		// Вставка данных пользователя в таблицу
 		_, err = db.Exec("INSERT INTO users (username, email, password) VALUES ($1, $2, $3)", user.Username, user.Email, user.Password)
@@ -54,6 +65,27 @@ func main() {
 		}
 
 		c.JSON(http.StatusOK, gin.H{"message": "User registered successfully"})
+	})
+
+	router.POST("/login", func(c *gin.Context) {
+		var req LoginRequest
+		if err := c.BindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
+			return
+		}
+		var user User
+		err := db.QueryRow("SELECT id, username, email, password FROM users WHERE username = $1", req.Username).Scan(&user.ID, &user.Username, &user.Email, &user.Password)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+			return
+		}
+
+		if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Login successful"})
 	})
 
 	// Запуск сервера на порту 8080
